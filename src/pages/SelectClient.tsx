d9cceba6563a254c9d3055b2c822b198f1cdbf07
lgaieta <lgaieta.dev@gmail.com>
2022-09-03 14:35:22 -0300

    Added Table to SelectClient page

--- src/pages/SelectClient.tsx
+++ src/pages/SelectClient.tsx
@@ -1,7 +1,35 @@
+import {
+    TableRow,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+} from '@mui/material';
 import Main from '../layouts/Main';
 
+const ClientExamples = ['Intel', 'CRSSCH', 'Aieta Consulting'];
+
 function SelectClient() {
-    return <Main title='Seleccione un cliente'>table</Main>;
+    return (
+        <Main title='Seleccione un cliente'>
+            <TableContainer component={Paper} variant='outlined'>
+                <Table>
+                    <TableHead>
+                        <TableCell>Nombre</TableCell>
+                    </TableHead>
+                    <TableBody>
+                        {ClientExamples.map(client => (
+                            <TableRow key={client}>
+                                <TableCell>{client}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Main>
+    );
 }
 
 export default SelectClient;
