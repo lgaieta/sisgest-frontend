8d16c7105ac8d4ddeac4d802dd91950362ea8292
lgaieta <lgaieta.dev@gmail.com>
2022-09-08 12:20:52 -0300

    Added table to empleados page

--- src/pages/empleados.page.tsx
+++ src/pages/empleados.page.tsx
@@ -1,7 +1,43 @@
+import {
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+} from '@mui/material';
 import Main from '../layouts/Main';
 
 function EmpleadosPage() {
-    return <Main title='Empleados'>Empleados</Main>;
+    return (
+        <Main title='Empleados'>
+            <TableContainer component={Paper} variant='outlined'>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Nombres</TableCell>
+                            <TableCell>Apellidos</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell>Luciano Gabriel</TableCell>
+                            <TableCell>Aieta</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>Luciano Gabriel</TableCell>
+                            <TableCell>Aieta</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>Luciano Gabriel</TableCell>
+                            <TableCell>Aieta</TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Main>
+    );
 }
 
 export default EmpleadosPage;
