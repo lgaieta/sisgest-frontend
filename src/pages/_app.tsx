f93030ab6940887d44e8efeb93f74d4eec0dd534
lgaieta <lgaieta.dev@gmail.com>
2022-12-02 21:40:55 -0300

    Moved meta tag from _document to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { Box, CssBaseline } from '@mui/material';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import { FC, useState } from 'react';
 import Sidebar from '../layouts/Sidebar';
 
@@ -7,6 +8,10 @@ const CustomApp: FC<AppProps> = ({ Component, pageProps }) => {
     const [client, setClient] = useState<string | null>('Visma');
     return (
         <>
+            <Head>
+                <meta charSet='UTF-8' />
+                <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+            </Head>
             <CssBaseline />
             <Box>
                 <Sidebar client={client} setClient={setClient} />
