237c0b1fc5e3b77807a644982b937957d312faea
lgaieta <lgaieta.dev@gmail.com>
2022-12-12 22:51:11 -0300

    Added title tag to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
+import Head from 'next/head';
 import Main from '../layouts/Main';
 
 function Home() {
-    return <Main title='Inicio'>Home page</Main>;
+    return (
+        <>
+            <Head>
+                <title>Inicio - SisGest</title>
+            </Head>
+            <Main title='Inicio'>Home page</Main>
+        </>
+    );
 }
 
 export default Home;
