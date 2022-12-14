1e3078014d5b5b5802334d340d775c36c016e33b
lgaieta <lgaieta.dev@gmail.com>
2022-12-14 12:25:56 -0300

    Added react-hook-form basic logic to CrearEmpleado

--- src/pages/empleados/crear.tsx
+++ src/pages/empleados/crear.tsx
@@ -1,7 +1,21 @@
+import { useForm } from 'react-hook-form';
+import Employee from '../../entities/Employee.entity';
 import Main from '../../layouts/Main';
 
+type EmployeeFormStructure = { names: Employee['names']; surnames: Employee['surnames'] };
+
 function CrearEmpleadoPage() {
-    return <Main title='Crear empleado'>Form</Main>;
+    const { register, handleSubmit } = useForm<EmployeeFormStructure>();
+
+    return (
+        <Main title='Crear empleado' hideHeader>
+            <form onSubmit={handleSubmit(data => console.log(data))}>
+                <input {...register('names')} />
+                <input {...register('surnames')} />
+                <input type='submit' />
+            </form>
+        </Main>
+    );
 }
 
 export default CrearEmpleadoPage;
