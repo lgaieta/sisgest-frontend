0f0ac686e986ac6e31825a33cbf22f1d9dd3a17d
lgaieta <lgaieta.dev@gmail.com>
2022-12-15 10:19:49 -0300

    Added maximum characters to CreateEmployee form

--- src/pages-content/empleados/crear/CreateEmployeeResolver.ts
+++ src/pages-content/empleados/crear/CreateEmployeeResolver.ts
@@ -4,10 +4,16 @@ import { zodResolver } from '@hookform/resolvers/zod';
 const schema = z.object({
     names: z
         .string()
-        .min(1, { message: 'El campo de los nombres no puede estar vacío.' }),
+        .min(1, { message: 'El campo de los nombres no puede estar vacío.' })
+        .max(100, {
+            message: 'El campo de los nombres no debe exceder los 100 caracteres.',
+        }),
     surnames: z
         .string()
-        .min(1, { message: 'El campo de los apellidos no puede estar vacío.' }),
+        .min(1, { message: 'El campo de los apellidos no puede estar vacío.' })
+        .max(100, {
+            message: 'El campo de los apellidos no debe exceder los 100 caracteres.',
+        }),
 });
 
 export const CreateEmployeeResolver = zodResolver(schema);
