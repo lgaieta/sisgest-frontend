07e3a30d2918e46f6a9e8ff7ee6c2db7363fe325
lgaieta <lgaieta.dev@gmail.com>
2022-12-16 11:46:30 -0300

    Added ErrorBoundary in Main layout

--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -1,5 +1,7 @@
 import { Box, AppBar, Typography, Toolbar } from '@mui/material';
 import { ReactNode } from 'react';
+import ErrorMessage from '../components/ErrorMessage';
+import ErrorBoundary from '../utils/ErrorBoundary';
 
 type MainProps = {
     title: string;
@@ -13,53 +15,61 @@ function Main(props: MainProps) {
     const { fullWidth = false, hideHeader = false } = props;
 
     return (
-        <Box
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                paddingLeft: fullWidth ? '0' : '13rem',
-                width: '100%',
-                boxSizing: 'border-box',
-            }}
+        <ErrorBoundary
+            fallbackComponent={
+                <ErrorMessage>
+                    Ha ocurrido un error al cargar la vista, por favor recargue la página.
+                </ErrorMessage>
+            }
         >
-            {hideHeader === true ? null : (
-                <AppBar
-                    component='header'
-                    sx={{
-                        bgcolor: 'background.paper',
-                        color: 'text.primary',
-                        borderLeft: 'none',
-                    }}
-                    elevation={0}
-                    position='sticky'
-                    variant='outlined'
-                >
-                    <Toolbar
-                        sx={{
-                            justifyContent: 'space-between',
-                        }}
-                    >
-                        <Typography
-                            variant='h4'
-                            component='h1'
-                            sx={{ fontWeight: '500' }}
-                        >
-                            {props.title}
-                        </Typography>
-                        {props.headerContent && props.headerContent()}
-                    </Toolbar>
-                </AppBar>
-            )}
             <Box
                 sx={{
-                    width: '100',
-                    minHeight: '100vh',
-                    padding: '1.5rem',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    paddingLeft: fullWidth ? '0' : '13rem',
+                    width: '100%',
+                    boxSizing: 'border-box',
                 }}
             >
-                {props.children}
+                {hideHeader === true ? null : (
+                    <AppBar
+                        component='header'
+                        sx={{
+                            bgcolor: 'background.paper',
+                            color: 'text.primary',
+                            borderLeft: 'none',
+                        }}
+                        elevation={0}
+                        position='sticky'
+                        variant='outlined'
+                    >
+                        <Toolbar
+                            sx={{
+                                justifyContent: 'space-between',
+                            }}
+                        >
+                            <Typography
+                                variant='h4'
+                                component='h1'
+                                sx={{ fontWeight: '500' }}
+                            >
+                                {props.title}
+                            </Typography>
+                            {props.headerContent && props.headerContent()}
+                        </Toolbar>
+                    </AppBar>
+                )}
+                <Box
+                    sx={{
+                        width: '100',
+                        minHeight: '100vh',
+                        padding: '1.5rem',
+                    }}
+                >
+                    {props.children}
+                </Box>
             </Box>
-        </Box>
+        </ErrorBoundary>
     );
 }
 
--- src/pages/empleados/crear.tsx
+++ src/pages/empleados/crear.tsx
@@ -23,48 +23,39 @@ function CreateEmployeePage() {
 
     return (
         <Main title='Crear empleado' hideHeader>
-            <ErrorBoundary
-                fallbackComponent={
-                    <ErrorMessage>
-                        Ha ocurrido un error al cargar la vista, por favor recargue la
-                        página.
-                    </ErrorMessage>
-                }
-            >
-                <Container>
-                    <Form onSubmit={handleSubmit(data => mutate(data))}>
-                        <Typography variant='h3' sx={{ fontWeight: '700' }}>
-                            Crear empleado
-                        </Typography>
-                        <TextField {...register('names')} label='Nombres' />
-                        <TextField {...register('surnames')} label='Apellidos' />
-                        <Button
-                            type='submit'
-                            variant='contained'
-                            size='large'
-                            disableElevation
-                            sx={{ padding: '0.75rem 1rem' }}
-                        >
-                            {isLoading ? 'Creando...' : ' Crear empleado'}
-                        </Button>
-                        {formState.errors ? (
-                            <>
-                                {Object.keys(formState.errors).map(errorName => (
-                                    <ErrorMessage key={errorName}>
-                                        {formState.errors[errorName].message as string}
-                                    </ErrorMessage>
-                                ))}
-                            </>
-                        ) : null}
-                        {isError && (
-                            <ErrorMessage>
-                                Ha ocurrido un error al crear el cliente, por favor
-                                intente nuevamente.
-                            </ErrorMessage>
-                        )}
-                    </Form>
-                </Container>
-            </ErrorBoundary>
+            <Container>
+                <Form onSubmit={handleSubmit(data => mutate(data))}>
+                    <Typography variant='h3' sx={{ fontWeight: '700' }}>
+                        Crear empleado
+                    </Typography>
+                    <TextField {...register('names')} label='Nombres' />
+                    <TextField {...register('surnames')} label='Apellidos' />
+                    <Button
+                        type='submit'
+                        variant='contained'
+                        size='large'
+                        disableElevation
+                        sx={{ padding: '0.75rem 1rem' }}
+                    >
+                        {isLoading ? 'Creando...' : ' Crear empleado'}
+                    </Button>
+                    {formState.errors ? (
+                        <>
+                            {Object.keys(formState.errors).map(errorName => (
+                                <ErrorMessage key={errorName}>
+                                    {formState.errors[errorName].message as string}
+                                </ErrorMessage>
+                            ))}
+                        </>
+                    ) : null}
+                    {isError && (
+                        <ErrorMessage>
+                            Ha ocurrido un error al crear el cliente, por favor intente
+                            nuevamente.
+                        </ErrorMessage>
+                    )}
+                </Form>
+            </Container>
         </Main>
     );
 }
