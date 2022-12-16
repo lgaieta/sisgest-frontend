4cf36cf659a2739c84108f5a436cc2bace507e08
lgaieta <lgaieta.dev@gmail.com>
2022-12-16 11:42:52 -0300

    Added ErrorBoundary to createEmployee form

--- src/pages/empleados/crear.tsx
+++ src/pages/empleados/crear.tsx
@@ -9,6 +9,7 @@ import { CreateEmployeeResolver } from '../../pages-content/empleados/crear/Crea
 import { postEmployee } from '../../services/postEmployee';
 import { useRouter } from 'next/router';
 const { Container, Form } = CreateEmployeeLayout;
+import ErrorBoundary from '../../utils/ErrorBoundary';
 
 function CreateEmployeePage() {
     const { formState, register, handleSubmit } = useForm<CreateEmployeeFormStructure>({
@@ -22,39 +23,48 @@ function CreateEmployeePage() {
 
     return (
         <Main title='Crear empleado' hideHeader>
-            <Container>
-                <Form onSubmit={handleSubmit(data => mutate(data))}>
-                    <Typography variant='h3' sx={{ fontWeight: '700' }}>
-                        Crear empleado
-                    </Typography>
-                    <TextField {...register('names')} label='Nombres' />
-                    <TextField {...register('surnames')} label='Apellidos' />
-                    <Button
-                        type='submit'
-                        variant='contained'
-                        size='large'
-                        disableElevation
-                        sx={{ padding: '0.75rem 1rem' }}
-                    >
-                        {isLoading ? 'Creando...' : ' Crear empleado'}
-                    </Button>
-                    {formState.errors ? (
-                        <>
-                            {Object.keys(formState.errors).map(errorName => (
-                                <ErrorMessage key={errorName}>
-                                    {formState.errors[errorName].message as string}
-                                </ErrorMessage>
-                            ))}
-                        </>
-                    ) : null}
-                    {isError && (
-                        <ErrorMessage>
-                            Ha ocurrido un error al crear el cliente, por favor intente
-                            nuevamente.
-                        </ErrorMessage>
-                    )}
-                </Form>
-            </Container>
+            <ErrorBoundary
+                fallbackComponent={
+                    <ErrorMessage>
+                        Ha ocurrido un error al cargar la vista, por favor recargue la
+                        página.
+                    </ErrorMessage>
+                }
+            >
+                <Container>
+                    <Form onSubmit={handleSubmit(data => mutate(data))}>
+                        <Typography variant='h3' sx={{ fontWeight: '700' }}>
+                            Crear empleado
+                        </Typography>
+                        <TextField {...register('names')} label='Nombres' />
+                        <TextField {...register('surnames')} label='Apellidos' />
+                        <Button
+                            type='submit'
+                            variant='contained'
+                            size='large'
+                            disableElevation
+                            sx={{ padding: '0.75rem 1rem' }}
+                        >
+                            {isLoading ? 'Creando...' : ' Crear empleado'}
+                        </Button>
+                        {formState.errors ? (
+                            <>
+                                {Object.keys(formState.errors).map(errorName => (
+                                    <ErrorMessage key={errorName}>
+                                        {formState.errors[errorName].message as string}
+                                    </ErrorMessage>
+                                ))}
+                            </>
+                        ) : null}
+                        {isError && (
+                            <ErrorMessage>
+                                Ha ocurrido un error al crear el cliente, por favor
+                                intente nuevamente.
+                            </ErrorMessage>
+                        )}
+                    </Form>
+                </Container>
+            </ErrorBoundary>
         </Main>
     );
 }
