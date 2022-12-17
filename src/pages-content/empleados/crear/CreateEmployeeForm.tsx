9a5bcc0066e115d18eedf2d26f14b63ea2a4556f
lgaieta <lgaieta.dev@gmail.com>
2022-12-17 14:05:21 -0300

    Moved CreateEmployeeForm to external file

---
+++ src/pages-content/empleados/crear/CreateEmployeeForm.tsx
@@ -0,0 +1,47 @@
+import { Typography, TextField, Button } from '@mui/material';
+import ErrorMessage from '../../../components/ErrorMessage';
+import { useCreateEmployeeForm } from './useCreateEmployeeForm';
+import CreateEmployeeLayout from '../../../layouts/employees/CreateEmployeeLayout';
+
+function CreateEmployeeForm() {
+    const { formState, register, handleSubmit, createEmployee, isLoading, isError } =
+        useCreateEmployeeForm();
+
+    return (
+        <CreateEmployeeFormLayout.Form
+            onSubmit={handleSubmit(data => createEmployee(data))}
+        >
+            <Typography variant='h3' sx={{ fontWeight: '700' }}>
+                Crear empleado
+            </Typography>
+            <TextField {...register('names')} label='Nombres' />
+            <TextField {...register('surnames')} label='Apellidos' />
+            <Button
+                type='submit'
+                variant='contained'
+                size='large'
+                disableElevation
+                sx={{ padding: '0.75rem 1rem' }}
+            >
+                {isLoading ? 'Creando...' : ' Crear empleado'}
+            </Button>
+            {formState.errors ? (
+                <>
+                    {Object.keys(formState.errors).map(errorName => (
+                        <ErrorMessage key={errorName}>
+                            {formState.errors[errorName].message as string}
+                        </ErrorMessage>
+                    ))}
+                </>
+            ) : null}
+            {isError && (
+                <ErrorMessage>
+                    Ha ocurrido un error al crear el cliente, por favor intente
+                    nuevamente.
+                </ErrorMessage>
+            )}
+        </CreateEmployeeFormLayout.Form>
+    );
+}
+
+export default CreateEmployeeForm;
--- src/pages/empleados/crear.tsx
+++ src/pages/empleados/crear.tsx
@@ -3,51 +3,19 @@ import Head from 'next/head';
 import ErrorMessage from '../../components/ErrorMessage';
 import CreateEmployeeLayout from '../../layouts/employees/CreateEmployeeLayout';
 import Main from '../../layouts/Main';
+import CreateEmployeeForm from '../../pages-content/empleados/crear/CreateEmployeeForm';
 import { useCreateEmployeeForm } from '../../pages-content/empleados/crear/useCreateEmployeeForm';
 
-const { Container, Form } = CreateEmployeeLayout;
+const { Container } = CreateEmployeeLayout;
 
 function CreateEmployeePage() {
-    const { formState, register, handleSubmit, createEmployee, isLoading, isError } =
-        useCreateEmployeeForm();
-
     return (
         <Main title='Crear empleado' hideHeader>
             <Head>
                 <title>Crear empleado - SisGest</title>
             </Head>
             <Container>
-                <Form onSubmit={handleSubmit(data => createEmployee(data))}>
-                    <Typography variant='h3' sx={{ fontWeight: '700' }}>
-                        Crear empleado
-                    </Typography>
-                    <TextField {...register('names')} label='Nombres' />
-                    <TextField {...register('surnames')} label='Apellidos' />
-                    <Button
-                        type='submit'
-                        variant='contained'
-                        size='large'
-                        disableElevation
-                        sx={{ padding: '0.75rem 1rem' }}
-                    >
-                        {isLoading ? 'Creando...' : ' Crear empleado'}
-                    </Button>
-                    {formState.errors ? (
-                        <>
-                            {Object.keys(formState.errors).map(errorName => (
-                                <ErrorMessage key={errorName}>
-                                    {formState.errors[errorName].message as string}
-                                </ErrorMessage>
-                            ))}
-                        </>
-                    ) : null}
-                    {isError && (
-                        <ErrorMessage>
-                            Ha ocurrido un error al crear el cliente, por favor intente
-                            nuevamente.
-                        </ErrorMessage>
-                    )}
-                </Form>
+                <CreateEmployeeForm />
             </Container>
         </Main>
     );
