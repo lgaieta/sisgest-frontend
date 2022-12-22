81774d8689bfc3d9b1fb07acdb3aed8e12fb724a
lgaieta <lgaieta.dev@gmail.com>
2022-12-22 19:37:39 -0300

    Removed unused imports in crear.tsx

--- src/pages/empleados/crear.tsx
+++ src/pages/empleados/crear.tsx
@@ -1,10 +1,7 @@
-import { TextField, Button, Typography } from '@mui/material';
 import Head from 'next/head';
-import ErrorMessage from '../../components/ErrorMessage';
 import CreateEmployeeLayout from '../../pages-content/empleados/crear/CreateEmployeeLayout';
 import Main from '../../layouts/Main';
 import CreateEmployeeForm from '../../pages-content/empleados/crear/CreateEmployeeForm';
-import { useCreateEmployeeForm } from '../../pages-content/empleados/crear/useCreateEmployeeForm';
 
 const { Container } = CreateEmployeeLayout;
 
