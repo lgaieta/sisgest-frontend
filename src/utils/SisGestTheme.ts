a2ebf76be96ce0922a2a0a6626f625c00c7d760c
lgaieta <lgaieta.dev@gmail.com>
2023-01-01 16:44:23 -0300

    Added responsive typography in theme

--- src/utils/SisGestTheme.ts
+++ src/utils/SisGestTheme.ts
@@ -1,18 +1,20 @@
 import { grey, pink } from '@mui/material/colors';
-import { createTheme } from '@mui/material/styles';
+import { createTheme, responsiveFontSizes } from '@mui/material/styles';
 
-const SisGestTheme = createTheme({
-    palette: {
-        primary: {
-            main: pink[600],
+const SisGestTheme = responsiveFontSizes(
+    createTheme({
+        palette: {
+            primary: {
+                main: pink[600],
+            },
+            secondary: {
+                main: grey[800],
+            },
+            background: {
+                default: grey[50],
+            },
         },
-        secondary: {
-            main: grey[800],
-        },
-        background: {
-            default: grey[50],
-        },
-    },
-});
+    })
+);
 
 export default SisGestTheme;
