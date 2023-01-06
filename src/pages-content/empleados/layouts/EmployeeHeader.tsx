3e2b5fb447bad15219246888b91328f5e550b20f
lgaieta <lgaieta.dev@gmail.com>
2023-01-06 19:41:14 -0300

    Added responsive header to employees page

--- src/pages-content/empleados/layouts/EmployeeHeader.tsx
+++ src/pages-content/empleados/layouts/EmployeeHeader.tsx
@@ -1,4 +1,4 @@
-import { Box, Tooltip, IconButton, Button } from '@mui/material';
+import { Box, Tooltip, IconButton, Button, AppBar, Toolbar } from '@mui/material';
 import Link from 'next/link';
 import ReplayIcon from '@mui/icons-material/Replay';
 import Header from '../../../layouts/header/Header';
@@ -11,7 +11,7 @@ const EmployeeHeader = ({
     onMenuIconClick: () => void;
 }) => (
     <Header title='Empleados' onMenuIconClick={onMenuIconClick}>
-        <Box sx={{ display: 'flex', gap: '1rem' }}>
+        <Box sx={{ display: { xs: 'none', sm: 'flex' }, gap: '1rem' }}>
             <Tooltip title='Recargar empleados'>
                 <IconButton onClick={() => onReplayIconClick()}>
                     <ReplayIcon sx={{ color: 'secondary.contrastText' }} />
@@ -26,6 +26,35 @@ const EmployeeHeader = ({
                 Crear Empleado
             </Button>
         </Box>
+        <AppBar
+            component='div'
+            sx={{
+                display: { xs: 'inherit', sm: 'none' },
+                bottom: '0',
+                top: 'auto',
+                backgroundColor: 'background.default',
+                color: 'black',
+            }}
+        >
+            <Toolbar>
+                <Box sx={{ display: 'flex', gap: '1rem', width: '100%' }}>
+                    <Button
+                        variant='contained'
+                        disableElevation
+                        component={Link}
+                        href='/empleados/crear'
+                        sx={{ width: '100%' }}
+                    >
+                        Crear Empleado
+                    </Button>
+                    <Tooltip title='Recargar empleados'>
+                        <IconButton onClick={() => onReplayIconClick()}>
+                            <ReplayIcon sx={{ color: 'text.primary' }} />
+                        </IconButton>
+                    </Tooltip>
+                </Box>
+            </Toolbar>
+        </AppBar>
     </Header>
 );
 
