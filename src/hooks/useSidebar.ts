edc0e02fdc549c69eabacb26cf8fc03663a885b4
lgaieta <lgaieta.dev@gmail.com>
2023-01-07 20:33:01 -0300

    Added close sidebar when route changes

--- src/hooks/useSidebar.ts
+++ src/hooks/useSidebar.ts
@@ -1,9 +1,13 @@
-import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import useClientSelector from './useClientSelector';
 
 const useSidebar = () => {
     const [client, setClient] = useClientSelector();
     const [open, setOpen] = useState<boolean>(false);
+    const router = useRouter();
+
+    useEffect(() => setOpen(false), [router.pathname]);
 
     return { open, setOpen, client, setClient };
 };
