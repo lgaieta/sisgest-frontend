e43ff37ff2c70741c3314900bb0e7cbb51e42eee
lgaieta <lgaieta.dev@gmail.com>
2023-01-31 16:11:19 -0300

    Removed old contracts in getContracts service

--- src/pages-content/contratos/services/getContractsFromRepository.ts
+++ src/pages-content/contratos/services/getContractsFromRepository.ts
@@ -6,29 +6,7 @@ function timeout(ms: number) {
 
 const getContractsFromRepository = async (): Promise<Contract[]> => {
     await timeout(3000);
-    return [
-        {
-            id: 1,
-            employee_id: 30,
-            start_date: 'Start Date',
-            finishing_date: 'Finishing Date',
-            status: 'ACTIVO',
-        },
-        {
-            id: 2,
-            employee_id: 33,
-            start_date: 'Start Date',
-            finishing_date: 'Finishing Date',
-            status: 'ACTIVO',
-        },
-        {
-            id: 3,
-            employee_id: 4,
-            start_date: 'Start Date',
-            finishing_date: 'Finishing Date',
-            status: 'ACTIVO',
-        },
-    ];
+    return [];
 };
 
 export default getContractsFromRepository;
