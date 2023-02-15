da226440d22f0d02019a1ee9cd67537eb779ab43
lgaieta <lgaieta.dev@gmail.com>
2023-02-15 19:40:22 -0300

    Added maxWidth to Content layout

--- src/layouts/content/Content.styles.tsx
+++ src/layouts/content/Content.styles.tsx
@@ -3,6 +3,7 @@ import { SxProps } from '@mui/material';
 
 export const ContentStyles: SxProps<Theme> = {
     width: '100%',
+    maxWidth: { md: 'calc(100vw - 13rem)' },
     height: '100%',
     gridArea: 'content',
     padding: '1.5rem',
