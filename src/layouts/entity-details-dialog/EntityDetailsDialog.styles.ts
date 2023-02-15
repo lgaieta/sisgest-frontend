290cfe21d78645c28b054fe1a7196e1208b74ad6
lgaieta <lgaieta.dev@gmail.com>
2023-02-15 19:56:53 -0300

    Added gap to EntityDetailsDialog grid

--- src/layouts/entity-details-dialog/EntityDetailsDialog.styles.ts
+++ src/layouts/entity-details-dialog/EntityDetailsDialog.styles.ts
@@ -2,6 +2,7 @@ const DetailsContainerStyles = {
     display: 'grid',
     gridTemplateColumns: 'repeat(auto-fill, minmax(225px, 1fr))',
     gridAutoRows: 'auto',
+    gap: '1rem',
     width: '100%',
 };
 
