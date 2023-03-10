90c78a17f4c43fe36605e623df32ab932a8e0bfb
lgaieta <lgaieta.dev@gmail.com>
2023-03-10 18:30:11 -0300

    Added tags to ReducedContractListTagsWithKeys

--- src/pages-content/contratos/utils/ReducedContractListTagsWithKeys.ts
+++ src/pages-content/contratos/utils/ReducedContractListTagsWithKeys.ts
@@ -1,8 +1,13 @@
 import Contract from '../../../entities/Contract.entity';
 
+// Los datos deben ser actualizados cuando el servicio esté hecho.
 const ReducedContractListTagsWithKeys: [string, keyof Contract][] = [
     ['Número de Contrato', 'id'],
+    ['Estado del Contrato', 'fechaAsignada'],
     ['Fecha de Inicio', 'fechaAsignada'],
+    ['Fecha Fin', 'fechaAsignada'],
+    ['Renovación del Contrato', 'fechaAsignada'],
+    ['Sueldo Mensual', 'fechaAsignada'],
 ];
 
 export default ReducedContractListTagsWithKeys;
