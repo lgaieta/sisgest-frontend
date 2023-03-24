1f351fbc77831cc17f50f85ac88e5a8070eb3adb
lgaieta <lgaieta.dev@gmail.com>
2023-03-24 18:50:13 -0300

    Moved LoginForm to /pages-content

---
+++ src/pages-content/iniciar-sesion/layouts/LoginForm.tsx
@@ -0,0 +1,46 @@
+import { Stack, Typography, TextField, Button } from '@mui/material';
+
+function LoginForm() {
+    return (
+        <Stack
+            component='form'
+            sx={{
+                gap: 2,
+                px: 6,
+                justifyContent: 'center',
+                bgcolor: 'background.secondary',
+                borderLeft: '1px solid',
+                borderColor: 'border.primary',
+            }}
+        >
+            <Typography variant='h4' component='h2' fontWeight={700}>
+                Iniciar sesión
+            </Typography>
+            <TextField
+                label='Usuario'
+                sx={{
+                    width: '100%',
+                    bgcolor: 'background.primary',
+                    borderRadius: '1rem',
+                }}
+            />
+            <TextField
+                label='Contraseña'
+                sx={{
+                    width: '100%',
+                    bgcolor: 'background.primary',
+                    borderRadius: '1rem',
+                }}
+            />
+            <Button
+                variant='contained'
+                size='large'
+                sx={{ fontSize: '1rem', py: '.75rem' }}
+            >
+                Ingresar
+            </Button>
+        </Stack>
+    );
+}
+
+export default LoginForm;
--- src/pages/iniciar-sesion.tsx
+++ src/pages/iniciar-sesion.tsx
@@ -3,6 +3,7 @@ import Button from '@mui/material/Button';
 import { useTheme } from '@mui/system';
 import Head from 'next/head';
 import Link from 'next/link';
+import LoginForm from '../pages-content/iniciar-sesion/layouts/LoginForm';
 
 function LoginPage() {
     const theme = useTheme();
@@ -58,44 +59,7 @@ function LoginPage() {
                     </Link>
                 </Typography>
             </Stack>
-            <Stack
-                component='form'
-                sx={{
-                    gap: 2,
-                    px: 6,
-                    justifyContent: 'center',
-                    bgcolor: 'background.secondary',
-                    borderLeft: '1px solid',
-                    borderColor: 'border.primary',
-                }}
-            >
-                <Typography variant='h4' component='h2' fontWeight={700}>
-                    Iniciar sesión
-                </Typography>
-                <TextField
-                    label='Usuario'
-                    sx={{
-                        width: '100%',
-                        bgcolor: 'background.primary',
-                        borderRadius: '1rem',
-                    }}
-                />
-                <TextField
-                    label='Contraseña'
-                    sx={{
-                        width: '100%',
-                        bgcolor: 'background.primary',
-                        borderRadius: '1rem',
-                    }}
-                />
-                <Button
-                    variant='contained'
-                    size='large'
-                    sx={{ fontSize: '1rem', py: '.75rem' }}
-                >
-                    Ingresar
-                </Button>
-            </Stack>
+            <LoginForm />
         </Box>
     );
 }
